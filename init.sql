9ebf89ddafc7cd7dd8ad8e5917bb232004713e3a
abap34 <abap0002@gmail.com>
2024-05-12 01:09:51 +0900

    add some dummy data

--- init.sql
+++ init.sql
@@ -11,7 +11,7 @@ USE app_db;
 
 CREATE TABLE posts (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    post_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    post_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     username VARCHAR(255) NOT NULL,
     public_score DOUBLE NOT NULL,
     private_score DOUBLE NOT NULL
@@ -23,3 +23,17 @@ INSERT INTO posts (username, public_score, private_score) VALUES ('user1', 0.5, 
 INSERT INTO posts (username, public_score, private_score) VALUES ('user2', 0.6, 0.6);
 INSERT INTO posts (username, public_score, private_score) VALUES ('user2', 0.7, 0.7);
 INSERT INTO posts (username, public_score, private_score) VALUES ('user4', 0.8, 0.8);
+
+
+INSERT INTO posts (post_date, username, public_score, private_score)
+VALUES (STR_TO_DATE('2024-05-10 00:00:00', '%Y-%m-%d %H:%i:%s'), 'abap34', 0.5, 0.5);
+INSERT INTO posts (post_date, username, public_score, private_score)
+VALUES (STR_TO_DATE('2024-05-10 01:00:00', '%Y-%m-%d %H:%i:%s'), 'abap34', 0.4, 0.6);
+INSERT INTO posts (post_date, username, public_score, private_score)
+VALUES (STR_TO_DATE('2024-05-10 02:00:00', '%Y-%m-%d %H:%i:%s'), 'abap34', 0.8, 0.7);
+INSERT INTO posts (post_date, username, public_score, private_score)
+VALUES (STR_TO_DATE('2024-05-10 03:00:00', '%Y-%m-%d %H:%i:%s'), 'abap34', 0.3, 0.8);
+INSERT INTO posts (post_date, username, public_score, private_score)
+VALUES (STR_TO_DATE('2024-05-11 05:00:00', '%Y-%m-%d %H:%i:%s'), 'abap34', 0.1, 0.5);
+INSERT INTO posts (post_date, username, public_score, private_score)
+VALUES (STR_TO_DATE('2024-05-11 16:00:00', '%Y-%m-%d %H:%i:%s'), 'abap34', 0.06, 0.5);
