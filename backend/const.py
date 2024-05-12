ee3b57f6f8a33026e51e5c5ac7f5326a65532cf3
abap34 <abap0002@gmail.com>
2024-05-13 01:54:24 +0900

    fix: change db name

--- backend/const.py
+++ backend/const.py
@@ -12,7 +12,7 @@ class Constants:
         "port": int(os.getenv("NS_MARIADB_PORT", 3306)),
         "user": os.getenv("NS_MARIADB_USER", "root"),
         "password": os.getenv("NS_MARIADB_PASSWORD", "password"),
-        "db": os.getenv("NS_MARIADB_DATABASE", "app_db"),
+        "db": "app_db",
         "charset": "utf8mb4",
         "cursorclass": pymysql.cursors.DictCursor,
     }
