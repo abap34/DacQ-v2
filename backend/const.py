90280bfe3fc34c6d081ab5295fce84024367ff2f
abap34 <abap0002@gmail.com>
2024-05-12 18:18:40 +0900

    format const

--- backend/const.py
+++ backend/const.py
@@ -1,23 +1,22 @@
 from typing import Literal
 import pymysql.cursors
 
+
 class Constants:
-    LABEL_PATH: str = 'data/label.csv'
-    LABEL_COL: str = 'label'
-    PRED_COL: str = 'pred'
-    ID_COL: str = 'id'
+    LABEL_PATH: str = "static/label.csv"
+    LABEL_COL: str = "label"
+    PRED_COL: str = "pred"
+    ID_COL: str = "id"
     DB_CONFIG = {
-        'host': 'mariadb',
-        'port': 3306,
-        'user': 'root',
-        'password': 'password',
-        'db': 'app_db',
-        'charset': 'utf8mb4',
-        'cursorclass': pymysql.cursors.DictCursor
+        "host": "mariadb",
+        "port": 3306,
+        "user": "root",
+        "password": "password",
+        "db": "app_db",
+        "charset": "utf8mb4",
+        "cursorclass": pymysql.cursors.DictCursor,
     }
 
-    SCORE_BETTERDIRECTION: Literal['smaller', 'larger'] = 'smaller'
+    SCORE_BETTERDIRECTION: Literal["smaller", "larger"] = "smaller"
 
-    SELCTED = ['LeaderBoard', 'Submit', 'Rules', 'Score Log']
-
-    TEAM_PATH = 'data/team_setting.csv'
+    TEAM_PATH = "static/team_setting.csv"
