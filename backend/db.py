db4d56d5a540c0eadab445ca73be101271f740de
abap34 <abap0002@gmail.com>
2024-05-12 12:37:13 +0900

    refactor: rename table name

--- backend/db.py
+++ backend/db.py
@@ -2,19 +2,20 @@ import pymysql.cursors
 import pandas as pd
 from const import Constants
 
+
 def get_submit() -> pd.DataFrame:
     # データベースに接続
     connection = pymysql.connect(**Constants.DB_CONFIG)
-    
+
     try:
         with connection.cursor() as cursor:
-            sql = "SELECT * FROM posts"
+            sql = "SELECT * FROM submitlog"
             cursor.execute(sql)
             result = cursor.fetchall()
-            
+
             df = pd.DataFrame(result)
             return df
-    
+
     finally:
         connection.close()
 
@@ -24,12 +25,9 @@ def add_submit(username: str, public_score: float, private_score: float):
 
     try:
         with connection.cursor() as cursor:
-            sql = "INSERT INTO posts (username, public_score, private_score) VALUES (%s, %s, %s)"
+            sql = "INSERT INTO submitlog (username, public_score, private_score) VALUES (%s, %s, %s)"
             cursor.execute(sql, (username, public_score, private_score))
             connection.commit()
 
     finally:
         connection.close()
-
-
-    
--- init.sql
+++ init.sql
@@ -1,17 +1,16 @@
-GRANT ALL PRIVILEGES ON *.* TO 'root' 
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' 
     IDENTIFIED BY 'password' 
     WITH GRANT OPTION;
 
 FLUSH PRIVILEGES;
 
 
-
 CREATE DATABASE IF NOT EXISTS app_db;
 USE app_db;
 
-CREATE TABLE posts (
+CREATE TABLE submitlog (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    post_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    post_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     username VARCHAR(255) NOT NULL,
     public_score DOUBLE NOT NULL,
     private_score DOUBLE NOT NULL
@@ -19,21 +18,22 @@ CREATE TABLE posts (
 
 
 -- ダミー
-INSERT INTO posts (username, public_score, private_score) VALUES ('user1', 0.5, 0.5);
-INSERT INTO posts (username, public_score, private_score) VALUES ('user2', 0.6, 0.6);
-INSERT INTO posts (username, public_score, private_score) VALUES ('user2', 0.7, 0.7);
-INSERT INTO posts (username, public_score, private_score) VALUES ('user4', 0.8, 0.8);
+INSERT INTO submitlog (username, public_score, private_score) VALUES ('user1', 0.5, 0.5);
+INSERT INTO submitlog (username, public_score, private_score) VALUES ('user2', 0.6, 0.6);
+INSERT INTO submitlog (username, public_score, private_score) VALUES ('user2', 0.7, 0.7);
+INSERT INTO submitlog (username, public_score, private_score) VALUES ('user4', 0.8, 0.8);
+INSERT INTO submitlog (username, public_score, private_score) VALUES ('Unknown', 0.03, 0.8);
 
 
-INSERT INTO posts (post_date, username, public_score, private_score)
+INSERT INTO submitlog (post_date, username, public_score, private_score)
 VALUES (STR_TO_DATE('2024-05-10 00:00:00', '%Y-%m-%d %H:%i:%s'), 'abap34', 0.5, 0.5);
-INSERT INTO posts (post_date, username, public_score, private_score)
+INSERT INTO submitlog (post_date, username, public_score, private_score)
 VALUES (STR_TO_DATE('2024-05-10 01:00:00', '%Y-%m-%d %H:%i:%s'), 'abap34', 0.4, 0.6);
-INSERT INTO posts (post_date, username, public_score, private_score)
+INSERT INTO submitlog (post_date, username, public_score, private_score)
 VALUES (STR_TO_DATE('2024-05-10 02:00:00', '%Y-%m-%d %H:%i:%s'), 'abap34', 0.8, 0.7);
-INSERT INTO posts (post_date, username, public_score, private_score)
+INSERT INTO submitlog (post_date, username, public_score, private_score)
 VALUES (STR_TO_DATE('2024-05-10 03:00:00', '%Y-%m-%d %H:%i:%s'), 'abap34', 0.3, 0.8);
-INSERT INTO posts (post_date, username, public_score, private_score)
+INSERT INTO submitlog (post_date, username, public_score, private_score)
 VALUES (STR_TO_DATE('2024-05-11 05:00:00', '%Y-%m-%d %H:%i:%s'), 'abap34', 0.1, 0.5);
-INSERT INTO posts (post_date, username, public_score, private_score)
+INSERT INTO submitlog (post_date, username, public_score, private_score)
 VALUES (STR_TO_DATE('2024-05-11 16:00:00', '%Y-%m-%d %H:%i:%s'), 'abap34', 0.06, 0.5);
