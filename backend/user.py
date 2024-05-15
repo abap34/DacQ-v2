ea5924a644dcf890e268623306471b9dc80b7c73
abap34 <abap0002@gmail.com>
2024-05-15 16:52:23 +0900

    Update user.py

--- backend/user.py
+++ backend/user.py
@@ -2,7 +2,6 @@ import streamlit as st
 from streamlit.web.server.websocket_headers import _get_websocket_headers
 
 
-@st.cache_data
 def get_username():
     headers = _get_websocket_headers()
     user = headers.get("X-Forwarded-User")
