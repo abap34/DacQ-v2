42eb72f5f80661e70d97923d4e9557280e96781d
abap34 <abap0002@gmail.com>
2024-07-10 05:41:11 +0900

    2024年　機械学習講習会コンペ用に設定を変更

--- backend/const.py
+++ backend/const.py
@@ -4,12 +4,13 @@ from typing import Literal
 
 import numpy as np
 import pymysql.cursors
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import accuracy_score
 
+from zoneinfo import ZoneInfo
 
 class Constants:
     LABEL_PATH: str = "static/label.csv"
-    LABEL_COL: str = "label"
+    LABEL_COL: str = "class"
     PRED_COL: str = "pred"
     ID_COL: str = "id"
     DB_CONFIG = {
@@ -22,28 +23,28 @@ class Constants:
         "cursorclass": pymysql.cursors.DictCursor,
     }
 
-    SCORE_BETTERDIRECTION: Literal["smaller", "larger"] = "smaller"
+    SCORE_BETTERDIRECTION: Literal["smaller", "larger"] = "larger"
 
     TEAM_PATH = "static/team_setting.csv"
     PUBLIC_PRIVATE_SETTING = "static/public_private_setting.csv"
     PRIVATE_SUBMIT_COUNT = 2
 
 
-
     DATASETS = {
-        "train-tiny": "https://abap34.com/trap_ml_lecture/train_tiny.csv",
         "train": "https://abap34.com/trap_ml_lecture/train.csv",
         "test": "https://abap34.com/trap_ml_lecture/test.csv",
+        "train-tiny": "https://abap34.com/trap_ml_lecture/train_tiny.csv",
+        "test-fillna": "https://abap34.com/trap_ml_lecture/test_fillna.csv",
         "sample_submission": "https://abap34.com/trap_ml_lecture/sample_submission.csv",
     }
 
     DATE = {
-        "public_start": datetime.datetime(2024, 7, 10, 18, 0, 0),
-        "public_end": datetime.datetime(2024, 7, 17, 0, 0, 0),
-        "private_start": datetime.datetime(2024, 7, 27, 17, 45, 0),
-        "private_end": datetime.datetime(2024, 9, 7, 0, 0, 0),
+        "public_start": datetime.datetime(2024, 7, 10, 18, 0, 0, tzinfo=ZoneInfo("Asia/Tokyo")),
+        "public_end": datetime.datetime(2024, 7, 16, 23, 59, 59, tzinfo=ZoneInfo("Asia/Tokyo")),
+        "private_start": datetime.datetime(2024, 7, 17, 17, 50, 0, tzinfo=ZoneInfo("Asia/Tokyo")),
+        "private_end": datetime.datetime(2024, 9, 7, 0, 0, 0, tzinfo=ZoneInfo("Asia/Tokyo"))
     }
 
     @staticmethod
     def score(y_true: np.ndarray, y_pred: np.ndarray) -> np.float64:
-        return mean_squared_error(y_true, y_pred)
+        return accuracy_score(y_true, y_pred)
