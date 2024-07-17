0d75bb2669b0b083e1fe80a4e8d6f9484a72a4f2
abap34 <abap0002@gmail.com>
2024-07-17 23:59:23 +0900

    refactor: 不要になった共有メッセージ作成の関数を削除

--- app/utils.py
+++ app/utils.py
@@ -128,9 +128,7 @@ def to_ranking(submitlog: pd.DataFrame, phase: Phase = Phase.public) -> pd.DataF
     # post_date が JST で保存されているので、そのままに
     lastsubmit_dates = lastsubmit_dates.dt.tz_localize(ZoneInfo("Asia/Tokyo"))
 
-    ranking["lastsubmit"] = ranking["teamid"].map(
-        now - lastsubmit_dates 
-    )
+    ranking["lastsubmit"] = ranking["teamid"].map(now - lastsubmit_dates)
 
     ranking["lastsubmit"] = ranking["lastsubmit"].apply(readable_timedelta)
 
@@ -200,65 +198,6 @@ def get_teamrank(submitlog: pd.DataFrame, teamname: str) -> int:
     ranking["rank"] = range(1, len(ranking) + 1)
 
     return ranking[ranking["teamname"] == teamname]["rank"].values[0]
-
-
-def get_sns_message(submitlog: pd.DataFrame, teamname: str) -> str:
-    ranking = get_teamrank(submitlog, teamname)
-
-    rank = ranking[ranking["teamname"] == teamname]
-
-    if rank.empty:
-        return f"DacQ に {teamname} として参加しています。あと {np.random.randint(1, 100)} 分以内に最初のサブミットをするつもりです。できなかったら本館前に埋めてもらってもいいですよ！"
-    else:
-        rank = rank["rank"].values[0]
-        message = get_sns_message_by_rank(rank)
-        return message
-
-
-def get_sns_message_by_rank(rank: int) -> str:
-    message = f"DacQ で現在 {rank} 位です!"
-
-    if rank == 1:
-        message += np.random.choice(
-            [
-                "誰もいない道を行く、王者はときには孤独になるものです",
-                "誰か追いつける人がいないと寂しいですね。",
-                "トップに立つことはいつだって特別です。私以外の人にとっては。",
-                "そろそろ順位を上げる喜びを味わってみたいものですね。",
-                "どこかのチームが抜いてくれないと退屈ですね。",
-                "追いかける人がいないと、何か物足りなさを感じます。",
-                "頂点に立つことは常に特別なことですが、それは私以外の人にとってです。",
-                "次の挑戦者が現れるのを楽しみにしています。",
-                "誰かが私を抜いてくれると刺激があっていいかもしれないですね。",
-            ]
-        )
-    elif rank == 2:
-        message += np.random.choice(
-            [
-                "私は順位を上げることができる最後の存在です。",
-                "順位を上げる余地がたくさんある皆さんが羨ましいです。"
-                "目の前の壁を乗り越えることが残りの楽しみです。",
-                "あと一つ背中を追い越せば、ついに望んだその順位です。",
-            ]
-        )
-    elif rank == 3:
-        message += np.random.choice(
-            ["まだアンサンブルしてないだけです", "アイデアはあります。"]
-        )
-    else:
-        message += np.random.choice(
-            [
-                "急に天啓が降ってこないかな〜",
-                "このコンペ、完全に理解しました。",
-                "ここだけの話、順位を上げる算段はついています。",
-                "こういうときは一旦、落ち着きましょう",
-                "あの辺のバグ直したら賞金圏内かな？",
-                "今のスコアを見てもしょうがないです。夢を見るなら先のスコアを見ましょう。",
-                "はじめから諦めてしまったら、それこそ終わりです。",
-            ]
-        )
-
-    return message
 
 
 def is_best_score(
